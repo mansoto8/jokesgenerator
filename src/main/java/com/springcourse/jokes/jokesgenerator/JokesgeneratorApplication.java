119fcb5e7e53882c58f71a84bf00de2f325ce21e
Juan Manuel <juan.soto@blackboard.com>
2020-08-29 21:37:39 -0500

    Project initialization

---
+++ src/main/java/com/springcourse/jokes/jokesgenerator/JokesgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.springcourse.jokes.jokesgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesgeneratorApplication.class, args);
+    }
+
+}
